4da102812eb4cfbaaed7d616a7513e0672ce0f41
Hugo Santos <hugosilva_santos@hotmail.com>
2015-03-30 13:05:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo npm cache clean
-sudonpm install --global yo bower grunt-cli
+sudo npm install --global yo bower grunt-cli
 sudo npm cache clean
 sudo npm install 
 bower install
